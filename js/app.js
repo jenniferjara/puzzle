644d0c74e3255d5735ad7b2706fab869a37ba8bc
jenniferjara <lisbet.jara@gmail.com>
2016-09-23 17:21:33 -0500

    cambios en el diseño

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 window.addEventListener("load", function() {
 	var box = document.getElementById("box");
+	//var boxUno = document.getElementById("boxUno");
+	//var boxDos = document.getElementById("boxDos");
 	var img = document.getElementById("img");
+	var imgUno = document.getElementById("imgUno");
+	var imgDos = document.getElementById("imgDos");
+
 	box.addEventListener("drop", function(ev){
 		ev.preventDefault();
 	    var data = ev.dataTransfer.getData("text");
@@ -11,8 +16,16 @@ window.addEventListener("load", function() {
 	});
 
 	img.addEventListener("dragstart", function(ev){
-		img.className = "w-100";
-		ev.dataTransfer.setData("text", ev.target.id);
+		imagen(ev);
 	});
-
+	imgUno.addEventListener("dragstart", function(ev){
+		imagen(ev);
+	});
+	imgDos.addEventListener("dragstart", function(ev){
+		imagen(ev);
+	});
+	
+	function imagen (ev){
+		ev.dataTransfer.setData("text", ev.target.id);
+	}
 });
