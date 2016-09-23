6bf0107ed3c25f63fa85bc1714f218d9a2e1f9d7
jenniferjara <lisbet.jara@gmail.com>
2016-09-23 13:27:51 -0500

    cambio de version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 window.addEventListener("load", function() {
-		
+	var box = document.getElementById("box");
+	var img = document.getElementById("img");
+	box.addEventListener("drop", function(ev){
+		ev.preventDefault();
+	    var data = ev.dataTransfer.getData("text");
+	    ev.target.appendChild(document.getElementById(data));
+	});
+	box.addEventListener("dragover", function(ev){
+		 ev.preventDefault();
+	});
+
+	img.addEventListener("dragstart", function(ev){
+		img.className = "w-100";
+		ev.dataTransfer.setData("text", ev.target.id);
+	});
+
 });
